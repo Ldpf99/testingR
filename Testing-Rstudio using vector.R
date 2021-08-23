6d9c6c763f8bc1cbd7656503d0399c9aaa089571
Ldpf99 <luis.daniel0729@gmail.com>
2021-08-22 21:49:45 -0500

    new

---
+++ Testing-Rstudio using vector.R
@@ -0,0 +1,7 @@
+#codes for creating vectors using c() function()
+
+z<-c(1,2) #numeric vector
+z<-c(TRUE,FALSE) #logical vector
+Z<-c("x","y","z") #character
+z<-1:10           #interger
+z<-c(1+01,2_11)   #complex vector
